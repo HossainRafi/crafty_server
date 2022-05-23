ab67d87ee8232246530705e7dbcf5ffb1b316a1b
HossainRafi <hossainrafi71@gmail.com>
2022-05-23 23:20:36 +0600

    all products api done

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-//================ Common Code Start =============
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
@@ -15,18 +14,32 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const productCollection = client.db("crafty").collection("product");
-  client.close();
-});
-//================ Common Code End =============
-// run().catch(console.dir);
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("crafty").collection("product");
 
+    // All Products API
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    // Single Product API
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+  } finally {
+    //    client.close();
+  }
+}
+run().catch(console.dir);
 
-
-
-// =====================================================================
 app.get("/", (req, res) => {
   res.send("Server Is Running....................");
 });
